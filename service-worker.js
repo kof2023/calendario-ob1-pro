e322d2d7bd73f639f45372f0e1832b03f232e8f0
kof2023 <149124302+kof2023@users.noreply.github.com>
2026-02-13 19:09:33 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
+const CACHE_NAME = 'ob1-v5';
+const ASSETS = [
+  './',
+  './index.html'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('ob1-store').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './assets/icon.png',
-    ])),
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
